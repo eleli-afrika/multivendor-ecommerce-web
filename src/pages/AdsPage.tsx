61fe10e4fb14c3b3b2082ee2cdc6418ec9176f0d
Jane49-cloud <janendirangu49@gmail.com>
2023-11-11 11:19:33 +0300

    debug

--- src/pages/AdsPage.tsx
+++ src/pages/AdsPage.tsx
@@ -1,15 +1,18 @@
 import { useSelector } from 'react-redux';
 import Popular from '../components/landing/popular';
 import Filters from '../constants/Filters';
+import Loader from '../constants/loader';
 
 const AdsPage = () => {
     const Ads = useSelector((state: any) => state.AllAds.SearchResults);
+    const isLoading = useSelector((state: any) => state.AllAds.isLoading);
     console.log(`Hello view my ads`, Ads);
 
     return (
         <div className="flex parent ">
             <Filters />
             <div className="flex-1 mx-auto  my-body p-5">
+                {isLoading && <Loader />}
                 {Ads?.length > 0 ? (
                     <Popular Ads={Ads} />
                 ) : (
