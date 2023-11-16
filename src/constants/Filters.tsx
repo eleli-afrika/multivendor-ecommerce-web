8299cd9ac5b3f6b872ea60d5f9b3a1c2a46aaec8
Jane49-cloud <janendirangu49@gmail.com>
2023-11-16 17:09:00 +0300

    Add filter options to filter sidebar

--- src/constants/Filters.tsx
+++ src/constants/Filters.tsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { SearchingProduct, setSearchResults } from '../Redux/slices/AdsSlice';
+import { useNavigate } from 'react-router-dom';
+import { AppDispatch } from '../Redux/store';
+
 const Filters = ({ Ads }: any) => {
-    // Extract unique categories
+    const [selectedPriceRange, setSelectedPriceRange] = useState<string | null>(null);
+    const dispatch = useDispatch<AppDispatch>();
+    const navigate = useNavigate();
+
+    const handlePriceRangeClick = (range: string) => {
+        setSelectedPriceRange(range);
+
+        const [min, max] = range.split(' - ').map((value) => parseInt(value.replace(/,/g, '')));
+        const filteredAds = Ads?.filter(
+            (ad: any) => ad.product_data.productprice >= min && ad.product_data.productprice <= max
+        );
+
+        console.log(filteredAds);
+        dispatch(setSearchResults(filteredAds));
+    };
+
+    const FilterAgain = (searchparam: any) => {
+        dispatch(SearchingProduct(searchparam));
+        navigate('/search/products');
+    };
+
     const categories = Array.from(new Set(Ads?.map((ad: any) => ad.product_data.category)));
-
-    // Extract unique brands
     const brands = Array.from(new Set(Ads?.map((ad: any) => ad.product_data.brand)));
-
-    // Calculate price ranges dynamically
     const calculatePriceRanges = () => {
         if (Ads?.length === 0) {
-            // Return some default ranges or an empty array based on your requirements
             return [];
         }
         if (Ads?.length === 1) {
@@ -53,7 +74,13 @@ const Filters = ({ Ads }: any) => {
                 <div className="scrollable-list">
                     <ul className="flex flex-wrap gap-2 text-[16px] text-gray-500">
                         {categories.map((category: any, index) => (
-                            <li key={index} className="border rounded-[16px] p-2">
+                            <li
+                                key={index}
+                                className="border rounded-[16px] p-2"
+                                onClick={() => {
+                                    FilterAgain(category);
+                                }}
+                            >
                                 {category}
                             </li>
                         ))}
@@ -69,7 +96,13 @@ const Filters = ({ Ads }: any) => {
                 <div className="scrollable-list">
                     <ul className="text-[16px] text-gray-500 cursor-pointer  flex flex-col gap-2 px-2 justify-center">
                         {priceRanges.map((priceRange, index) => (
-                            <li key={index}>{priceRange}</li>
+                            <li
+                                key={index}
+                                onClick={() => handlePriceRangeClick(priceRange)}
+                                className={selectedPriceRange === priceRange ? 'selected' : ''}
+                            >
+                                {priceRange}
+                            </li>
                         ))}
                     </ul>
                 </div>
@@ -83,7 +116,14 @@ const Filters = ({ Ads }: any) => {
                 <div className="text-[16px] text-gray-500 cursor-pointer  flex flex-col gap-2 px-2 justify-center">
                     <ul className="space-y-2">
                         {brands.map((brand: any, index) => (
-                            <li key={index}>{brand}</li>
+                            <li
+                                key={index}
+                                onClick={() => {
+                                    FilterAgain(brand);
+                                }}
+                            >
+                                {brand}
+                            </li>
                         ))}
                     </ul>
                 </div>
