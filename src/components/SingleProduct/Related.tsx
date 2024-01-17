10953df78bdde21e2414dfe66f86d520e83cab87
Jane49-cloud <janendirangu49@gmail.com>
2024-01-17 05:59:33 +0300

    debug

--- src/components/SingleProduct/Related.tsx
+++ src/components/SingleProduct/Related.tsx
@@ -1,44 +1,40 @@
-import { useSelector } from 'react-redux';
-import Productcard from '../Global/PopularCard';
+import { useSelector } from "react-redux";
+import Productcard from "../Global/PopularCard";
 
 const Related = () => {
-    const Ads = useSelector((state: any) => state.ad.similarAds);
-    // console.log(Ads);
+  const Ads = useSelector((state: any) => state.ad.similarAds);
+  // console.log(Ads);
 
-    function formatPriceWithCommas(price: any) {
-        if (price) {
-            return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-        }
-        return ''; // or any default value you prefer if price is undefined
+  function formatPriceWithCommas(price: any) {
+    if (price) {
+      return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
-    return (
-        <div className="lg:p-5 ">
-            <div className="pb-2  bg-gray-light ">
-                <div className="py-3  flex flex-row items-center justify-between px-5 md:justify-center">
-                    <h1 className="text-stone-700">You may also like</h1>
-                    {/* <button className="underline rounded-lg px-2 text-sm py-1 text-slate-500">
-                      
-                    </button> */}
-                </div>
-
-                <div className="flex flex-wrap items-center justify-center lg:justify-normal">
-                    {Ads?.map((product: any) => (
-                        <>
-                            <Productcard
-                                key={product.producttid}
-                                image={`${product?.mainimage}`}
-                                name={product?.productname}
-                                price={formatPriceWithCommas(product?.productprice)}
-                                // seller={product?.user_name}
-                                id={product?.producttid}
-                                // description={product?.product_data?.productdescription}
-                            />
-                        </>
-                    ))}
-                </div>
-            </div>
+    return ""; // or any default value you prefer if price is undefined
+  }
+  return (
+    <div className="py-2 mt-2  max-w-7xl mx-auto ">
+      <div className="py-3 px-[20px]  flex flex-row items-center justify-between bg-gray-light my-3 rounded-t-[8px] ">
+        <h1 className="text-black-main my-2 font-bold">You May also Like</h1>
+      </div>
+      <div className="pb-2  bg-white px-[5px] shadow-lg">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 w-full py-4">
+          {Ads?.map((product: any) => (
+            <>
+              <Productcard
+                key={product.producttid}
+                image={`${product?.mainimage}`}
+                name={product?.productname}
+                price={formatPriceWithCommas(product?.productprice)}
+                // seller={product?.user_name}
+                id={product?.producttid}
+                // description={product?.product_data?.productdescription}
+              />
+            </>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Related;
