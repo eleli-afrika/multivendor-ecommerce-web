4002f2ba1eeeaac6df5b04ab66f77589a727aea3
Jane49-cloud <janendirangu49@gmail.com>
2024-01-17 11:24:49 +0300

    debug

--- src/components/Dashboard/Inquiries.tsx
+++ src/components/Dashboard/Inquiries.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { GetInquiries } from '../../Redux/hooks/inquiry';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Delete, FiberManualRecord, Visibility } from '@mui/icons-material';
 
 const Inquiries = () => {
     const [inquiry, setInquiry] = useState([]);
@@ -19,22 +20,33 @@ const Inquiries = () => {
     }, []);
 
     return (
-        <div className="px-8 py-20 max-w-6xl mx-auto bg-gray-light h-[100vh]">
+        <div className="px-8 py-20 max-w-6xl mx-auto bg-white h-[100vh] shadow-lg">
             {inquiry.map((item: any) => (
                 <ul
                     key={item.id}
-                    className={`max-w-2xl mb-4 p-4 bg-black-main  ${
-                        item.read ? '' : ''
-                    } text-white rounded-md`}
+                    className={`max-w-2xl mb-2 bg-white text-black-main text-[16px] rounded-md shadow-lg  border-t border-t-gray-100  ${
+                        item.read ? 'border-l-3  border-green-500' : 'border-l-4 border-blue-500'
+                    }`}
                 >
-                    <Link to={'/'} className="block">
-                        <p> {item.message}</p>
-                        <p>{new Date(item.createdAt).toLocaleDateString()}</p>
-                    </Link>
+                    <li className="flex justify-between items-center py-3 px-[10px] rounded-[8px]">
+                        <Link to={'/'} className="flex-1 pr-4">
+                            <p className="font-bold">{item.message}</p>
+                            <p className="text-gray-500">
+                                {new Date(item.createdAt).toLocaleDateString()}
+                            </p>
+                        </Link>
+                        <div className="flex items-center space-x-2">
+                            {item.read ? (
+                                <Visibility className="text-green-500" />
+                            ) : (
+                                <FiberManualRecord className="text-blue-500" />
+                            )}
+                            <Delete className="text-red-600" />
+                        </div>
+                    </li>
                 </ul>
             ))}
         </div>
     );
 };
-
 export default Inquiries;
