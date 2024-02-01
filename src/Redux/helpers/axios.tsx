ef8cb3fc43d16e29f1ed855e1715f482bf279538
Jane49-cloud <janendirangu49@gmail.com>
2024-02-01 14:14:18 +0300

    debug

--- src/Redux/helpers/axios.tsx
+++ src/Redux/helpers/axios.tsx
@@ -1,15 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const axiosService = axios.create({
-  baseURL: "https://dev.eleliafrika.com",
-  // baseURL: 'https://localhost:8000',
-  // baseURL: 'https://34.93.14.27:8000',
+    baseURL: 'https://dev.eleliafrika.com',
+    // baseURL: 'http://localhost:8000',
+    // baseURL: 'https://34.93.14.27:8000',
 });
 
 axiosService.interceptors.request.use(async (req) => {
-  let token = localStorage.getItem("userToken");
-  if (token) {
-    req.headers["x-access-token"] = token;
-  }
-  return req;
+    let token = localStorage.getItem('userToken');
+    if (token) {
+        req.headers['x-access-token'] = token;
+    }
+    return req;
 });
