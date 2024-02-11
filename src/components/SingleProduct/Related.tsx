dd43a0884520feb4f34e3941f0bc65a196cb52f2
Jane49-cloud <janendirangu49@gmail.com>
2024-02-12 02:23:03 +0300

    debug

--- src/components/SingleProduct/Related.tsx
+++ src/components/SingleProduct/Related.tsx
@@ -20,7 +20,7 @@ const Related = () => {
             </div>
             <div className="pb-2  bg-white px-[5px] shadow-lg">
                 {isLoading ? (
-                    <div className="flex flex-wrap">
+                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2">
                         <ProductLoader count={6} />
                     </div>
                 ) : (
