6936496c6153ad47293078a9b6c0c398e33066da
kamah <kamah@localhost.localdomain>
2025-01-17 11:52:36 +0300

    first commit

--- src/Redux/helpers/axios.tsx
+++ src/Redux/helpers/axios.tsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export const axiosService = axios.create({
-    baseURL: 'https://api.eduka.co.ke',
+    // baseURL: 'https://api.eduka.co.ke',
+     baseURL: 'https://ecommerce.eleliafrika.cloud',
     // baseURL: 'http://localhost:8000',
-    // baseURL: 'https://137.184.189.199:8000',
+    // baseURL: 'http://194.195.215.228:8082',
 });
 
 axiosService.interceptors.request.use(async (req) => {
