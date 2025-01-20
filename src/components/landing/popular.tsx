fffc8ae6ea5030dcec68227f8b43282a42c35308
Jane49-cloud <janendirangu49@gmail.com>
2025-01-20 23:53:30 +0300

    UPdated products code

--- src/components/landing/popular.tsx
+++ src/components/landing/popular.tsx
@@ -1,28 +1,28 @@
-import Productcard from '../Global/PopularCard';
+import Productcard from "../Global/PopularCard";
 
 const Popular = ({ Ads }: any) => {
-    function formatPriceWithCommas(price: any) {
-        if (price) {
-            return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-        }
-        return '';
+  function formatPriceWithCommas(price: any) {
+    if (price) {
+      return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
+    return "";
+  }
 
-    return (
-        <div className="px-4 w-full overflow-hidden bg-white shadow-lg py-4">
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2">
-                {Ads?.map((product: any) => (
-                    <Productcard
-                        key={product.product_data?.producttid}
-                        image={product.product_data?.mainimage}
-                        name={product.product_data?.productname}
-                        price={formatPriceWithCommas(product?.product_data?.productprice)}
-                        id={product?.product_data?.producttid}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="px-4 w-full overflow-hidden bg-white shadow-lg py-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2">
+        {Ads?.map((product: any) => (
+          <Productcard
+            key={product?.producttid}
+            image={product?.mainimage}
+            name={product?.productname}
+            price={formatPriceWithCommas(product?.productprice)}
+            id={product?.producttid}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Popular;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,84 +1,87 @@
-import Sidebar from '../constants/sidebar';
-import ImageSlider from '../components/landing/slider';
-import Popular from '../components/landing/popular';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getLoggedInUser } from '../Redux/slices/AuthSlice';
+import Sidebar from "../constants/sidebar";
+import ImageSlider from "../components/landing/slider";
+import Popular from "../components/landing/popular";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getLoggedInUser } from "../Redux/slices/AuthSlice";
 // import Minslider from '../components/landing/minislider';
 // import Sponsered from '../components/landing/Sponsered';
 
 import {
-    FetchProductsAsync,
-    FetchSponsoredProductsAsync,
-    FetchTopProductsAsync,
-} from '../Redux/slices/AdsSlice';
-import { AppDispatch } from '../Redux/store';
-import { GetInquiries } from '../Redux/hooks/inquiry';
-import CardHeader from '../components/Global/CardHeader';
-import ProductLoader from '../components/Global/ProductLoader';
+  FetchProductsAsync,
+  FetchSponsoredProductsAsync,
+  FetchTopProductsAsync,
+} from "../Redux/slices/AdsSlice";
+import { AppDispatch } from "../Redux/store";
+import { GetInquiries } from "../Redux/hooks/inquiry";
+import CardHeader from "../components/Global/CardHeader";
+import ProductLoader from "../components/Global/ProductLoader";
 
 const Landing = () => {
-    const userToken = useSelector((state: any) => state.auth.userToken);
-    const dispatch = useDispatch<AppDispatch>();
-    // const { Ads, SponseredAds, TopAds } = useSelector((state: any) => state.AllAds);
-    const { Ads, isLoading } = useSelector((state: any) => state.AllAds);
-    const [, setInquiry] = useState([]);
-    const user = useSelector((state: any) => state.auth.user);
+  const userToken = useSelector((state: any) => state.auth.userToken);
+  const dispatch = useDispatch<AppDispatch>();
+  // const { Ads, SponseredAds, TopAds } = useSelector((state: any) => state.AllAds);
+  const { Ads, isLoading } = useSelector((state: any) => state.AllAds);
+  console.log(Ads);
+  const [, setInquiry] = useState([]);
+  const user = useSelector((state: any) => state.auth.user);
 
-    useEffect(() => {
-        document.title = 'eDuka';
-    }, []);
-    useEffect(() => {
-        dispatch(getLoggedInUser());
-    }, [dispatch]);
+  useEffect(() => {
+    document.title = "eDuka";
+  }, []);
+  useEffect(() => {
+    dispatch(getLoggedInUser());
+  }, [dispatch]);
 
-    const getUser = async () => {
-        dispatch(getLoggedInUser());
+  const getUser = async () => {
+    dispatch(getLoggedInUser());
+  };
+
+  // fetch products
+  useEffect(() => {
+    dispatch(FetchProductsAsync());
+  }, []);
+
+  useEffect(() => {
+    dispatch(FetchTopProductsAsync());
+  }, []);
+
+  useEffect(() => {
+    dispatch(FetchSponsoredProductsAsync());
+  }, []);
+
+  useEffect(() => {
+    if (userToken) {
+      getUser();
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await GetInquiries();
+      const data = response.data;
+      const usersInquiries = data.filter(
+        (item: any) => item.user === user?.userid
+      );
+      setInquiry(usersInquiries);
+      // console.log(inquiry);
     };
 
-    // fetch products
-    useEffect(() => {
-        dispatch(FetchProductsAsync());
-    }, []);
+    fetch();
+  }, []);
+  return (
+    <div>
+      <div className="bg-gray-light">
+        <div className="flex parent ">
+          <Sidebar />
+          <div className="flex-1 mx-auto  my-body ">
+            <ImageSlider />
+          </div>
+        </div>
 
-    useEffect(() => {
-        dispatch(FetchTopProductsAsync());
-    }, []);
-
-    useEffect(() => {
-        dispatch(FetchSponsoredProductsAsync());
-    }, []);
-
-    useEffect(() => {
-        if (userToken) {
-            getUser();
-        }
-    }, []);
-
-    useEffect(() => {
-        const fetch = async () => {
-            const response = await GetInquiries();
-            const data = response.data;
-            const usersInquiries = data.filter((item: any) => item.user === user?.userid);
-            setInquiry(usersInquiries);
-            // console.log(inquiry);
-        };
-
-        fetch();
-    }, []);
-    return (
-        <div>
-            <div className="bg-gray-light">
-                <div className="flex parent ">
-                    <Sidebar />
-                    <div className="flex-1 mx-auto  my-body ">
-                        <ImageSlider />
-                    </div>
-                </div>
-
-                <div className="py-2 mt-2  max-w-7xl mx-auto ">
-                    <div className="mx-0 lg:mx:auto">
-                        {/* <CardHeader cardName="Top Ads" linkTitle={'See All'} link={'/top_ads'} />
+        <div className="py-2 mt-2  max-w-7xl mx-auto ">
+          <div className="mx-0 lg:mx:auto">
+            {/* <CardHeader cardName="Top Ads" linkTitle={'See All'} link={'/top_ads'} />
                         <Minslider Ads={TopAds} />
                         <CardHeader
                             cardName="Sponsored Ads"
@@ -86,24 +89,24 @@ const Landing = () => {
                             link={'/sponsored_ads'}
                         />
                         <Sponsered Ads={SponseredAds} /> */}
-                        <CardHeader
-                            cardName="All Ads Listing"
-                            linkTitle={'See All'}
-                            link={'/all_ads'}
-                        />
-                        {isLoading ? (
-                            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2">
-                                {' '}
-                                <ProductLoader count={6} />
-                            </div>
-                        ) : (
-                            <Popular Ads={Ads} />
-                        )}
-                    </div>
-                </div>
-            </div>
+            <CardHeader
+              cardName="All Ads Listing"
+              linkTitle={"See All"}
+              link={"/all_ads"}
+            />
+            {isLoading ? (
+              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2">
+                {" "}
+                <ProductLoader count={6} />
+              </div>
+            ) : (
+              <Popular Ads={Ads} />
+            )}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
