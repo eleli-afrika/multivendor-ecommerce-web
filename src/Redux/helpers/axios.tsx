73eca6e1dc373516de5110d5f68983f78794b1e2
eleli-ayub <eleliayub@eleliafrika.com>
2026-02-23 08:09:54 +0300

    corrected the cicd to only build and push image to registry and not publish to server

--- src/Redux/helpers/axios.tsx
+++ src/Redux/helpers/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosService = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  baseURL: import.meta.env.API_URL
 });
 
 axiosService.interceptors.request.use(async (req) => {
--- src/Redux/slices/ApiSlice.tsx
+++ src/Redux/slices/ApiSlice.tsx
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  baseUrl: import.meta.env.API_URL
 });
 
 export const apiSlice = createApi({
